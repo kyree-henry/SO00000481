f30bf1cb68deb2b3dcbd42d990cf5850ad1b406f
Kyree henry <ene.henry.eh@gmail.com>
2024-12-06 12:44:45 +0100

    Updated: added functionality for update user feature and registered it

--- src/modules/user.module.ts
+++ src/modules/user.module.ts
@@ -12,14 +12,16 @@ import { UserRepository } from "../infrastructure/repositories/user.repository";
 import { RoleRepository } from '../infrastructure/repositories/role.repository';
 import { GetUserController } from "../features/user/get-user/get-user.endpoint";
 import { GetUsersController } from "../features/user/get-users/get-users.endpoint";
+import { UpdateUserHandler } from '../features/user/update-user/update-user.handler';
 import { CreateUserHandler } from "../features/user/create-user/create-user.handler";
 import { UserRoleRepository } from '../infrastructure/repositories/userRole.repository';
 import { CreateUserController } from "../features/user/create-user/create-user.endpoint";
+import { UpdateUserController } from '../features/user/update-user/update-user.endpoint';
 
 @Module({
   imports: [CqrsModule, TypeOrmModule.forFeature([User, Role, RoleClaim, UserRole])],
-  controllers: [CreateUserController, GetUsersController, GetUserController],
-  providers: [JwtService, CreateUserHandler, GetUsersHandler, GetUserHandler,
+  controllers: [CreateUserController, UpdateUserController, GetUsersController, GetUserController],
+  providers: [JwtService, CreateUserHandler, UpdateUserHandler, GetUsersHandler, GetUserHandler,
     {
       provide: 'IUserRepository',
       useClass: UserRepository,
